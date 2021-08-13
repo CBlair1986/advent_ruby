d6339462499da0649f8a74766776971b85900a75
harblcat <CBlair1986@gmail.com>
2021-08-12 22:33:31 -0400

    Starting on day2 part1, getting the building blocks in place

--- day2.rb
+++ day2.rb
@@ -4,4 +4,13 @@ puts input
 
 # Part 1
 
+# Parsing passwords:
+# The password is presented on the same line as its validator sequence.
+# If the given string is "1-9 a: abcde" then it wants to verify that anywhere between one and nine 'a's are present in the ending string.
+
+def parse_min_max(str)
+	return false, str
+end
+
+
 # Part 2
