0cfe4a3f630c1e7ac9a6f38b6921dedafb7a098a
harblcat <CBlair1986@gmail.com>
2021-08-13 21:21:18 -0400

    crude parsing for password parts, woo

--- day2.rb
+++ day2.rb
@@ -3,8 +3,6 @@
 # File input
 input = File.read('input/day2.txt')
 
-puts input
-
 # Part 1
 
 # Parsing passwords:asdf
@@ -12,8 +10,36 @@ puts input
 # If the given string is "1-9 a: abcde" then it wants to verify that anywhere between one and nine 'a's are present in
 # the ending string.
 
-def parse_min_max(str)
-  [false, str]
+def tokens(str)
+  str.split
 end
 
+def parse_min_max(tokens)
+  first = tokens.shift
+  x, y = first.split '-'
+
+  [[x, y], tokens]
+end
+
+def parse_character(tokens)
+  first = tokens.shift
+  char = first[0]
+
+  [char, tokens]
+end
+
+parse_results = input.lines.map do |line|
+  ts = tokens(line)
+  min_maxes, rest = parse_min_max(ts)
+  min, max = min_maxes
+  character, rest = parse_character(rest)
+  {
+    min: min,
+    max: max,
+    character: character,
+    password: rest[0]
+  }
+end
+
+pp parse_results
 # Part 2
