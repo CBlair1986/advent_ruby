714f4dd93757286f0efdef8631601b38eed5e1aa
harblcat <CBlair1986@gmail.com>
2021-08-15 11:32:53 -0400

    part 1 done, starting on part 2

--- day4.rb
+++ day4.rb
@@ -2,6 +2,30 @@
 
 input = File.read('input/day4.txt')
 
-chunks = input.split("\n\n").map(&:lines)
+chunks = input.split("\n\n").map(&:lines).map do |chunk|
+  c = {}
+  chunk.map(&:strip).join(' ').split.each do |part|
+    p1, p2 = part.split(':')
+    c[p1.to_sym] = p2
+  end
+  c
+end
 
-pp chunks
+# chunks now contains hashes of each entry, which will be convenient.
+
+# Part 1
+chunks.select! do |chunk|
+  if chunk.count == 8
+    true
+  elsif chunk.count == 7 && !chunk[:cid]
+    true
+  else
+    false
+  end
+end
+
+puts "Part 1: #{chunks.count}"
+
+# Part 2
+
+# Requires data validation
