6e11759d3a10a0fc843d0ab8db5b5f1d4b4c66d0
harblcat <CBlair1986@gmail.com>
2021-08-15 12:38:14 -0400

    finally found the stupid error, part 2 done

--- day4.rb
+++ day4.rb
@@ -29,3 +29,69 @@ puts "Part 1: #{chunks.count}"
 # Part 2
 
 # Requires data validation
+
+RULES = {
+  byr: lambda { |str|
+    num = str.to_i
+    num >= 1920 && num <= 2002
+  },
+  iyr: lambda { |str|
+    num = str.to_i
+    num >= 2010 && num <= 2020
+  },
+  eyr: lambda { |str|
+    num = str.to_i
+    num >= 2020 && num <= 2030
+  },
+  hgt: lambda { |str|
+    s = str.clone
+    units = s.slice!(-2, 2)
+    num = s.to_i
+
+    case units
+    when 'in'
+      num >= 59 && num <= 76
+    when 'cm'
+      num >= 150 && num <= 193
+    else
+      false
+    end
+  },
+  hcl: lambda { |str|
+    chars = str.chars
+    if chars.shift == '#'
+      s = chars.join
+      s.count('1234567890abcdef') == 6
+    else
+      false
+    end
+  },
+  ecl: lambda { |str|
+    %w[amb blu brn gry grn hzl oth].member? str
+  },
+  pid: lambda { |str|
+    str.count('1234567890') == 9
+  },
+  cid: lambda { |_str|
+    true
+  }
+}.freeze
+
+REQUIRED = %w[byr iyr eyr hgt hcl ecl pid].map(&:to_sym).freeze
+
+def validate(chunk)
+  if REQUIRED.all? { |sym| chunk[sym] }
+    results = chunk.map do |(k, v)|
+      puts "#{k}, #{v}" unless RULES[k.to_sym][v]
+      RULES[k.to_sym][v]
+    end
+
+    results.all?
+  else
+    false
+  end
+end
+
+num_valid = chunks.select { |chunk| validate(chunk) }.count
+
+puts "Part 2: #{num_valid}"
