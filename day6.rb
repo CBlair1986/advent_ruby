5ba73b838f7236befc28b29c6e51c83e5e922f18
harblcat <CBlair1986@gmail.com>
2021-08-16 20:53:25 -0400

    ezclap, day 6 part 2 done

--- day6.rb
+++ day6.rb
@@ -29,3 +29,23 @@ end
 pp g
 
 puts "Part 1: #{groups_sum(g)}"
+
+# Part 2
+
+# Should be just a simple change here
+
+def group_count2(group)
+  sets = group.map { |g| Set.new(g.chars) }
+  intersection = sets.reduce(&:intersection)
+  intersection.count
+end
+
+def groups_sum2(groups)
+  total = 0
+  groups.each do |group|
+    total += group_count2(group)
+  end
+  total
+end
+
+puts "Part 2: #{groups_sum2(g)}"
