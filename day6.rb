747a15577f85ced5f13e2a4cdd249f1f76d35796
harblcat <CBlair1986@gmail.com>
2021-08-16 20:50:21 -0400

    Day 6 part 1 done

--- day6.rb
+++ day6.rb
@@ -2,3 +2,30 @@
 
 input = File.read('input/day6.txt')
 
+# Part 1
+
+def groups(str)
+  # Assume str is groups of letters, separated by a blank line
+  str.split("\n\n").map(&:split)
+end
+
+g = groups(input)
+
+def group_count(group)
+  sets = group.map { |g| Set.new(g.chars) }
+  union = sets.reduce(Set.new, &:union)
+
+  union.count
+end
+
+def groups_sum(groups)
+  total = 0
+  groups.each do |group|
+    total += group_count(group)
+  end
+  total
+end
+
+pp g
+
+puts "Part 1: #{groups_sum(g)}"
