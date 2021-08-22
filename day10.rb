525afb67ba653333c78dfd345340f3606fc33668
harblcat <CBlair1986@gmail.com>
2021-08-22 16:12:31 -0400

    tinkering

--- day10.rb
+++ day10.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
-input = File.read('input/day10.txt')
+input = File.read('input/day10.txt').lines.map(&:to_i)
+
+# Looks like we're daisy-chaining adaptors. Input is a list of them.
+# We need to use all the adaptors, then count the number of 1-jolt differences and the number of 3-jolt differences, before finally multiplying them together.
