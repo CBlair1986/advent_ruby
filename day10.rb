004144efd0b21fbf434f16a3aaee58b87a759c13
harblcat <CBlair1986@gmail.com>
2021-08-22 16:23:39 -0400

    finished part 1, after an off-by-one error twice

--- day10.rb
+++ day10.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
-input = File.read('input/day10.txt').lines.map(&:to_i)
+# Part 1
+
+input = File.read('input/day10.txt').lines.map(&:to_i).sort
+lagged_input = input.clone
+lagged_input.shift
+
+zipped_inputs = input.zip(lagged_input)
+zipped_inputs.pop # This should be pairs of inputs now.
+
+one_gaps = zipped_inputs.select { |(one, two)| two - one == 1 }
+three_gaps = zipped_inputs.select { |(one, two)| two - one == 3 }
 
 # Looks like we're daisy-chaining adaptors. Input is a list of them.
-# We need to use all the adaptors, then count the number of 1-jolt differences and the number of 3-jolt differences, before finally multiplying them together.
+# We need to use all the adaptors, then count the number of 1-jolt differences and the number of 3-jolt differences,
+# before finally multiplying them together.
+
+# pp input, one_gaps, one_gaps.count, three_gaps, three_gaps.count
+
+puts "Part 1: #{(one_gaps.count + 1) * (three_gaps.count + 1)}"
