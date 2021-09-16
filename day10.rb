f4e628e0f0f25befe0577ffd7651e3f9baec35df
harblcat <CBlair1986@gmail.com>
2021-09-16 17:48:16 -0400

    still stumped

--- day10.rb
+++ day10.rb
@@ -79,46 +79,146 @@ part_two = input.clone
 # Let's try a graph
 
 # DirectedGraph provides an API to interact with a graph structure.
-# class DirectedGraph
-#   def initialize
-#     @vertices = Set.new
-#     @edges = Set.new
-#   end
+class DirectedGraph
+  def initialize(input, cover)
+    @vertices = Set.new
+    @edges = Set.new
 
-#   def add_vertex(value)
-#     @vertices.add value
-#   end
+    input.each do |n|
+      children = input.select(&cover.curry.call(n))
+      children.each do |m|
+        add_edge n, m
+      end
+    end
+  end
 
-#   def add_edge(value, another_value)
-#     add_vertex value
-#     add_vertex another_value
-#     @edges.add [value, another_value]
-#   end
+  def add_vertex(value)
+    @vertices.add value
+  end
 
-#   def get_connections(value)
-#     @edges.select { |a, _b| a == value }
-#   end
-# end
+  def include?(value)
+    @vertices.include? value
+  end
 
-# g = DirectedGraph.new
+  def max
+    @vertices.max
+  end
 
-# input.each do |n|
-#   children = input.select { |num| (1..3).cover? num - n }
-#   children.each do |m|
-#     g.add_edge n, m
-#   end
-# end
+  def min
+    @vertices.min
+  end
 
-# start = 0
-# target = input.max
+  def size
+    @vertices.count
+  end
 
-# def follow_connections(value, target, g)
-#   return 1 if value == target
+  def add_edge(value, another_value)
+    add_vertex value
+    add_vertex another_value
+    @edges.add [value, another_value]
+  end
 
-#   connections = g.get_connections(value).map { |_a, b| b }
-#   connections.map { |v| follow_connections v, target, g }.reduce(&:+)
-# end
+  def get_connections_from(value)
+    @edges.select { |a, _b| a == value }
+  end
 
-# puts follow_connections(start, target, g)
+  def get_connections_to(value)
+    @edges.select { |_a, b| b == value }
+  end
 
-p(part_two.each_cons(2).map { |a, b| b - a }.chunk { |n| n == 3 }.to_a)
+  def inspect
+    @edges.to_a.sort
+  end
+end
+
+COVER_FUNC = ->(n, num) { (1..3).cover? num - n }
+
+graph = DirectedGraph.new(input, COVER_FUNC)
+
+# I should be able to separate the graph into individual graphs, if I just loop through the nodes and "split" each one
+# that only has two links...
+
+# DirectedGraphProcessor is going to be doing the heavy lifting here...
+class DirectedGraphProcessor
+  def initialize(graph)
+    @graph = graph
+  end
+
+  def flood_fill(from, steps = 1)
+    return [] unless @graph.include? from
+
+    visited = []
+    edge = []
+
+    visited.unshift from
+
+    connections = @graph.get_connections_from from
+    connections.map { |e| e[1] }.each do |e|
+      edge.unshift e
+    end
+
+    while steps.positive?
+      # Iterate through the edge set, testing if any destinations are in visited, and if not, putting them into edge instead.
+      current_edge = edge.clone
+      edge = []
+
+      current_edge.each do |node|
+        next if visited.include? node
+
+        visited.unshift node
+
+        connections = @graph.get_connections_from node
+        connections.map { |e| e[1] }.each do |e|
+          edge.unshift e
+        end
+      end
+      steps -= 1
+    end
+    visited
+  end
+
+  def partition_graph
+    subgraphs = []
+
+    # The thinking here is that through successive flood filling we should be able to determine when we've hit a
+    # bottleneck as soon as the next flood fill step only adds one node to the 'visited' nodes.
+    old_total = new_total = 0
+    starting_node = @graph.min
+    graph_end = @graph.max
+
+    until starting_node == graph_end
+      chunk = []
+      (1..@graph.size).each do |i|
+        old_total = new_total
+        chunk = flood_fill starting_node, i
+        new_total = chunk.count
+        break if new_total - old_total == 1
+      end
+      subgraphs.push chunk
+      starting_node = chunk.max
+    end
+    subgraphs.map { |coll| DirectedGraph.new(coll, COVER_FUNC) }
+  end
+end
+
+dpg = DirectedGraphProcessor.new graph
+
+subgraphs = dpg.partition_graph
+
+def follow_connections(value, target, graph)
+  return 1 if value == target
+
+  connections = graph.get_connections_from(value).map { |(_a, b)| b }
+  connections.map { |val| follow_connections val, target, graph }.reduce(&:+)
+end
+
+lengths = subgraphs.map do |graph|
+  start = graph.min
+  target = graph.max
+
+  [graph, follow_connections(start, target, graph)]
+end
+
+pp lengths
+
+# puts "Part 2: #{lengths.reduce(&:*)}"
