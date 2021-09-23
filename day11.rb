551ff79f327c9932195299f528acb09fa86c5155
harblcat <CBlair1986@gmail.com>
2021-09-23 13:14:51 -0400

    for some reason it's not working correctly

--- day11.rb
+++ day11.rb
@@ -32,14 +32,16 @@ class Cell
     @state == :taken
   end
 
-  def inspect
-    @state
+  def to_s
+    { open: 'L',
+      closed: '.',
+      taken: '#' }[@state]
   end
 end
 
 # Field captures the required behavior for running the life simulation.
 class Field
-  attr_accessor :field
+  attr_accessor :field, :changed
 
   def initialize(value)
     @field = []
@@ -47,6 +49,10 @@ class Field
       @field[idx] = []
       parse_line line, idx
     end
+
+    @height = @field.count
+    @width = @field[0].count
+    @changed = true
   end
 
   def parse_line(line, idx)
@@ -64,8 +70,13 @@ class Field
     # Get the neighbors of the given cell location
     result = []
     locations = ([-1, 0, 1] * 2).permutation(2).uniq.to_a
+    locations.delete [0, 0]
     locations.each do |(y, x)|
-      result.push(@field[y + idx][x + jdx])
+      line = @field[y + idx]
+      next if line.nil?
+
+      cell = @field[y + idx][x + jdx]
+      result.push(cell) unless cell.nil?
     end
     result
   end
@@ -77,6 +88,41 @@ class Field
   def filled_neighbors(idx, jdx)
     neighbors(idx, jdx).select(&:taken?)
   end
+
+  def occupied
+    @field.flatten.select(&:taken?).count
+  end
+
+  def step
+    @changed = false
+    new_field = Marshal.load(Marshal.dump(@field))
+
+    @height.times do |i|
+      @width.times do |j|
+        seat = @field[i][j]
+        new_field[i][j] = Cell.open if seat.taken? && (filled_neighbors(i, j).count >= 4)
+        new_field[i][j] = Cell.taken if seat.open? && filled_neighbors(i, j).count.zero?
+      end
+    end
+
+    @changed = true unless @field.join == new_field.join
+
+    @field = new_field
+  end
+
+  def inspect
+    @field.map(&:join).join "\n"
+  end
 end
 
-p Field.new input
+f = Field.new input
+
+pp f
+
+while f.changed
+  f.step
+  pp f
+  puts f.occupied
+end
+
+p f.occupied
