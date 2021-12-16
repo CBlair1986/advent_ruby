6c884b5b8832cdf5a58e354d8f7ab40a04b9e57d
harblcat <CBlair1986@gmail.com>
2021-12-16 17:46:13 -0500

    tweak file arrangement

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,6 @@ task :default do
   FileList['day*.rb'].each { |file| ruby file }
 end
 
-task :day, [:day] do |_t, args|
-  FileList["day#{args.day}.rb"].each { |file| ruby file }
+task :day, [:year, :day] do |_t, args|
+  FileList["./#{args.year}/day#{args.day}.rb"].each { |file| ruby file }
 end
