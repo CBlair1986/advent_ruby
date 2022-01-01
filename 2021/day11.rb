4407cfd5ab3ab29df676647e4126b2c8c6c3746a
harblcat <CBlair1986@gmail.com>
2022-01-01 12:22:31 -0500

    starting day11

--- 2021/day11.rb
+++ 2021/day11.rb
@@ -1,1 +1,35 @@
 # frozen_string_literal: true
+
+input = File.read('input/day11.txt')
+            .lines
+            .map(&:strip)
+            .map(&:chars)
+            .map { |chars| chars.map(&:to_i) }
+
+## Grid will handle the grid, obviously
+class Grid
+  def initialize(grid)
+    @grid = grid
+    @height = grid.length
+    @width = grid.first.length
+  end
+
+  def at(xloc, yloc)
+    @grid[yloc][xloc]
+  end
+
+  def ortho_neighbors(xloc, yloc)
+    # Returns the orthographic (n,s,e,w) neighbors
+  end
+
+  def diag_neighbors(xloc, yloc)
+    # Returns the diagonal (ne, nw, se, sw) neighbors
+  end
+
+  def neighbors(xloc, yloc)
+    # Returns both the diagonal and orthographic neighbors
+    ortho_neighbors(xloc, yloc) | diag_neighbors(xloc, yloc)
+  end
+end
+
+g = Grid.new(input)
