714c856a44fbecafe4eff8b3dec2145f3ec5d48e
harblcat <CBlair1986@gmail.com>
2022-10-09 12:26:42 -0400

    finished 2015 day 1

--- 2015/day01.rb
+++ 2015/day01.rb
@@ -6,12 +6,23 @@ input = File.read('input/day01.txt').chars()
 
 level = 0
 
+position = 1
+
+basement_position = nil
+
 input.each do |c|
 	if c == '(' then
 		level += 1
 	else
 		level -= 1
 	end
+
+	if level == -1 && basement_position == nil then
+		basement_position = position
+	end
+
+	position += 1
 end
 
-puts level
+puts "Part 1: We end on level #{level}"
+puts "Part 2: We enter the basement at #{basement_position}"
