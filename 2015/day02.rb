70d136ce0320f1b3192a49cf552af8d199917c05
harblcat <CBlair1986@gmail.com>
2022-10-09 13:08:57 -0400

    completed 2015 day 2

--- 2015/day02.rb
+++ 2015/day02.rb
@@ -11,8 +11,19 @@ def extract_area(s)
 	(2*l*w) + (2*w*h) + (2*h*l) + (l*w)
 end
 
+def extract_ribbon_length(s)
+	parts = s.split("x")
+	unless parts.count > 0
+		return
+	end
+
+	parts = parts.map(&:to_i).sort
+	l, w, h = parts
+	(l+l+w+w) + (l*w*h)
+end
+
 input = File.read('input/day02.txt')
 			.lines()
-			.map { |line| extract_area line }
 
-puts input.reduce(&:+)
+puts "Part 1: We need #{input.map { |line| extract_area line }.reduce(&:+)} square feet of wrapping paper."
+puts "Part 2: We need #{input.map { |line| extract_ribbon_length line }.reduce(&:+)} feet of ribbon."
