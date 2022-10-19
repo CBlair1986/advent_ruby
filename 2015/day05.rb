6601305b93b44229dad9b9fd2274b9fe29a6015e
harblcat <CBlair1986@gmail.com>
2022-10-18 22:22:00 -0400

    starting 2015 day 5

---
+++ 2015/day05.rb
@@ -0,0 +1,27 @@
+#frozen_string_literal: true
+
+input = File.read('input/day05.txt').lines
+
+def three_vowels?(s)
+  s.scan(/[aeiou]/).count >= 3
+end
+
+def double_letter?(s)
+  s.scan(/(.)\1/).count.positive?
+end
+
+def bad_strings?(s)
+  %w[ab cd pq xy].map{ |pair|
+    s.include? pair
+  }.any?
+end
+
+def nice?(s)
+  three_vowels?(s) && double_letter?(s) && !bad_strings?(s)
+end
+
+nice_strings_count = input.filter { |s|
+  nice? s
+}.count
+
+puts "Part 1: there are #{nice_strings_count} nice strings"
