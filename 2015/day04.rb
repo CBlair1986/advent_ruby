3676146a23b1df58df2aaebe31573625ffd77eaa
harblcat <CBlair1986@gmail.com>
2022-10-18 20:10:30 -0400

    attempt at 2015 day 4

--- 2015/day04.rb
+++ 2015/day04.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
+require 'digest'
+
 input = File.read('input/day04.txt').strip
+
+def leading_zeros(s)
+  s.chars.take_while {|c| c == '0'}.count
+end
+
+def find_number(s)
+  salt = 1
+
+  plaintext = s + salt.to_s
+
+  while leading_zeros(Digest::MD5.hexdigest(plaintext)) < 5
+    salt += 1
+    plaintext = s + salt.to_s
+  end
+  salt
+end
+
+salt = find_number(input)
+
+puts "Part 1: the salt is #{salt}"
